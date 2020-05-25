0714a4aabe9ec7944076ee80e62835b90ed7b126
minhyuk <dufflaa@gmail.com>
2020-05-26 01:25:04 +0900

    Add schedule sending message intevally

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+Use Python 3.8.0
+Ubuntu 18.04
+
 1. source venv/bin/activate
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import os
+import schedule
+import time
 from slacker import Slacker
 
 token = os.environ['SLACKER_KEY'] #export SLACKER_KEY = xoxb-~~~~
 slack = Slacker(token)
 
-slack.chat.post_message("#random", "반가워!!")
+chatroom = "#random"
+
+# def send_message(chatroom, message):
+    # slack.chat.post_message(chatroom, message)
+    # slack.chat.post_message(chatroom, "사랑해!")
+    # slack.chat.post_message("#random", "사랑해!")
+
+def send_message():
+    slack.chat.post_message("#random", "사랑해!")
+
+schedule.every(10).seconds.do(send_message)
+
+while True: 
+    schedule.run_pending()
+    time.sleep(1)
